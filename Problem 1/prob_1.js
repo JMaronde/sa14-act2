96267800981d12d4dd6478599e8e6bcd4ccbb5d5
Jonathan Maronde <j2mike4@gmail.com>
2024-04-06 13:08:05 -0500

    Added preventDefault to my js file, so I would stop getting the undefined error in my html page

--- Problem 1/prob_1.js
+++ Problem 1/prob_1.js
@@ -1,12 +1,14 @@
 // fetch data from WeatherAPI.com, current weather and forecast for the next 4 days
 
 const button = document.getElementsByTagName("button")[0]
-button.addEventListener("click", async function fetchData() {
+button.addEventListener("click", async function fetchData(event) {
+    event.preventDefault();
+
     try{
         const api_key = "da5e14a9da384504818201700240304"
         const city_name = document.getElementById("cityInput").value;
-        const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city_name}&days=5`).then((response) => response.json())
-
+        const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city_name}&days=5`).then((response) => response.json());
+        
         console.log(data);
 
     }
