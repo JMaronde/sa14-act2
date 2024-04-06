8b4788b2ead15404c864fc88ee3749380df24d4a
Jonathan Maronde <j2mike4@gmail.com>
2024-04-06 15:33:08 -0500

    Wrote the rest of the code in the js file to fetch the correct weather info for 5 days.

--- Problem 1/prob_1.js
+++ Problem 1/prob_1.js
@@ -9,8 +9,26 @@ button.addEventListener("click", async function fetchData(event) {
         const city_name = document.getElementById("cityInput").value;
         const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city_name}&days=5`).then((response) => response.json());
         
-        console.log(data);
+        var days = document.querySelectorAll('.day'); // gets each day div
 
+        for (var i = 0; i < days.length; i++) { // iterate through each of them
+
+            const date = data.forecast.forecastday[i].date; // grab the needed weather info from the API
+            const icon = data.forecast.forecastday[i].day.condition.icon;
+            const max_temp = data.forecast.forecastday[i].day.maxtemp_f;
+            const min_temp = data.forecast.forecastday[i].day.mintemp_f;
+            const humidity = data.forecast.forecastday[i].day.avghumidity;
+
+            var day = days[i];
+            
+            day.innerHTML = ` 
+            <h2>${date}</h2>
+            <img src="https:${icon}">
+            <p>Max Temp: ${max_temp} degrees F</p>
+            <p>Min Temp: ${min_temp} degrees F</p>
+            <p>Humidity: ${humidity}%</p>           
+            `
+        }
     }
     catch(error) {
         console.log("Error fetching data: ", error);
