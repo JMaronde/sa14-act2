7e2c671265c1a3693c047b6c9fc26c56b4adfddd
Jonathan Maronde <j2mike4@gmail.com>
2024-04-07 21:00:24 -0500

    Added code in the javascript to have and edit button next to each list item, so the user can edit the task

--- Problem 3/prob_3.js
+++ Problem 3/prob_3.js
@@ -8,6 +8,16 @@ function add_item(event) {
 
         list_item.textContent = task.value;
 
+        // creating the edit button 
+        const edit_button = document.createElement("button");
+        edit_button.textContent = "Edit";
+
+        edit_button.onclick = function(event) {
+            event.preventDefault();
+            const newTask = prompt("Enter the new task:"); // prompts the user to write the new task
+            list_item.textContent = newTask;
+            }
+
         // creating the delete button
         const delete_button = document.createElement("button");
         delete_button.textContent = "Delete";
@@ -17,11 +27,12 @@ function add_item(event) {
             const index = Array.from(todo_list.children).indexOf(list_item); // gets the index of the current list item
             todo_list.removeChild(todo_list.children[index]); // removes the list item
             todo_list.removeChild(delete_button); // removes the delete button
+            todo_list.removeChild(edit_button); // removes the delete button
         }
 
         todo_list.appendChild(list_item);  // adds the new list item to the ordered list
-        todo_list.appendChild(delete_button) // adds a delete button next the the new list item
-
+        todo_list.appendChild(edit_button); // adds an edit button next the the new list item
+        todo_list.appendChild(delete_button); // adds a delete button next the the new list item
 
     }
     catch(error) {
