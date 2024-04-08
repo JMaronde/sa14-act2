9f489b9231cedcbc9087f3b92d6fe14df066081d
Jonathan Maronde <j2mike4@gmail.com>
2024-04-07 20:48:42 -0500

    Added code in the javascript to have a delete button next to each list element

--- Problem 3/prob_3.js
+++ Problem 3/prob_3.js
@@ -7,7 +7,21 @@ function add_item(event) {
         const list_item = document.createElement("li");
 
         list_item.textContent = task.value;
+
+        // creating the delete button
+        const delete_button = document.createElement("button");
+        delete_button.textContent = "Delete";
+        
+        delete_button.onclick = function delete_item(event) {
+            event.preventDefault();
+            const index = Array.from(todo_list.children).indexOf(list_item); // gets the index of the current list item
+            todo_list.removeChild(todo_list.children[index]); // removes the list item
+            todo_list.removeChild(delete_button); // removes the delete button
+        }
+
         todo_list.appendChild(list_item);  // adds the new list item to the ordered list
+        todo_list.appendChild(delete_button) // adds a delete button next the the new list item
+
 
     }
     catch(error) {
