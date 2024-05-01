a870e82b8baada9892dd0852db892fdf65f1e0a3
Jonathan Maronde <j2mike4@gmail.com>
2024-04-30 20:13:12 -0500

    Updated the api key in problem 1 to be a new one that can forecast 5 days

--- Problem 1/prob_1.js
+++ Problem 1/prob_1.js
@@ -5,7 +5,7 @@ button.addEventListener("click", async function fetchData(event) {
     event.preventDefault();
 
     try{
-        const api_key = "da5e14a9da384504818201700240304"
+        const api_key = "2bdfdc032d3e48ff8bf123502242204"
         const city_name = document.getElementById("cityInput").value;
         const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city_name}&days=5`).then((response) => response.json());
         
